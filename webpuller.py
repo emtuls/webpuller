b394c2b8a25fe0f1a7d759a49ddeda7706a878f5
emtuls <emtuls@gmail.com>
2019-04-17 18:13:48 -0400

    added file include option, to limit files wanting to be pulled. Also fixed error if directory detected

--- webpuller.py
+++ webpuller.py
@@ -2,6 +2,7 @@
 
 # pip3 install requests
 # pip3 install beautifulsoup4
+import argparse
 import requests
 import shutil
 import urllib
@@ -12,12 +13,16 @@ from bs4 import BeautifulSoup
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("USAGE: " + sys.argv[0] + " <URL>")
-        sys.exit(1)
-    url = sys.argv[1]
+    parser = argparse.ArgumentParser()
+    parser.add_argument("URL", type=str, 
+                        help="URL you want to pull files from.")
+    parser.add_argument("-i", "--Include", type=str,
+                        help="Text file that lists only files wanting to be pulled.")
+    args = parser.parse_args()
+    url = args.URL
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+        }
     r = requests.get(url, headers=headers)
     data = r.text
     soup = BeautifulSoup(data)
@@ -27,9 +32,16 @@ if __name__ == '__main__':
     for link in soup.find_all('a'):
         fileName = link.get('href')
         fixedName = urllib.parse.unquote_plus(fileName)
-        if (fileName == '../' or fileName == url):
+        if fixedName == '../' or fixedName == url:
             continue
+        if (args.Include):
+            with open(args.Include, 'r') as inFile:
+                files = inFile.read()
+                if fixedName not in files:
+                    continue
+			
         req = Request(url + fileName, headers=headers)
-
-        with urllib.request.urlopen(req) as response, open(outDir + fixedName, 'wb') as out_file:
-            shutil.copyfileobj(response, out_file)
+        
+        if not (outDir + fixedName).endswith('/'):
+            with urllib.request.urlopen(req) as response, open(outDir + fixedName, 'wb') as out_file:
+                shutil.copyfileobj(response, out_file)
